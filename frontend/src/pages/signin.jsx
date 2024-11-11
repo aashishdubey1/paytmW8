2e8b9e8c8053908f78809a6a00d2ae64bee8d918
Aashish <aashishdubey.dev@gmail.com>
2024-11-12 03:44:36 +0530

    feat: added signin page

--- frontend/src/pages/signin.jsx
+++ frontend/src/pages/signin.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Heading from '../components/Heading'
+import SubHeading from '../components/SubHeading'
+import InputBox from '../components/InputBox'
+import ButtonComp from '../components/Button'
+import ButtonWarning from '../components/ButtonWarning'
 
 const Signin = () => {
   return (
-    <div>signin</div>
+    <div className='h-screen w-full flex justify-center items-center bg-neutral-400'>
+        <section className='rounded bg-white w-74 px-8 py-4 '>
+            <Heading label={"Sign In"} />
+            <SubHeading label={"Enter your credentials to access your account"} />
+            <InputBox label={"Email"} placeholder={"johndoe@example.com"} />
+            <InputBox label={"Password"} placeholder={""} />
+            <ButtonComp label={"Sign in"} />
+            <ButtonWarning label={"Don't have an account ?"} linktext={"Sign up"} to={"/signup"} />
+        </section>
+    </div>
   )
 }
 
